df58566d8af3dc650a632a9af11dec97553bad62
idtaysaev <taysaevid@gmail.com>
2019-02-19 19:57:19 +0300

    d6b57e6

---
+++ precache-manifest.7c81601c8b8b7565b8041baa3d5b175e.js
@@ -0,0 +1,282 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2eaedbef345a4a813d4dce01c2985e6",
+    "url": "/img/plus.f2eaedbe.svg"
+  },
+  {
+    "revision": "e453ece4b5bd55aad2ea9b5a85deba6f",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "05400631a60974d8d92a",
+    "url": "/js/chunk-vendors.496b2173.js"
+  },
+  {
+    "revision": "670a057a7630f348c259a8f70df20a8c",
+    "url": "/img/test/giveaway.png"
+  },
+  {
+    "revision": "64da8f0f3dc3312cada38d60f5e46fef",
+    "url": "/img/wheel-me.svg"
+  },
+  {
+    "revision": "d56763b695365f77540ee2b53a8f6274",
+    "url": "/img/subscribers.d56763b6.svg"
+  },
+  {
+    "revision": "f71d20196d4caf35b6a670db8c70b03d",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "2e13dd6b6ababdd408b2e8eb33b350b1",
+    "url": "/img/no-avatar.png"
+  },
+  {
+    "revision": "1a26c30cbc4d988b612455e9a510d243",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb343f5ab78d443cea10bc42cd50d8a0",
+    "url": "/img/wheel-me.bb343f5a.svg"
+  },
+  {
+    "revision": "546a95dc9e2bd0576a9c9e5f175f8f68",
+    "url": "/img/sex.546a95dc.svg"
+  },
+  {
+    "revision": "a0b30edfba1fba8f0c525075d05d9f31",
+    "url": "/img/popular-reg.a0b30edf.svg"
+  },
+  {
+    "revision": "8d2bcb925e74b92dec980b14444a9748",
+    "url": "/img/wallet.8d2bcb92.svg"
+  },
+  {
+    "revision": "91ba773aec702a5956537262a0485532",
+    "url": "/img/promotion.91ba773a.svg"
+  },
+  {
+    "revision": "7a26dadb1de5adb3f2c913ae67d0e97b",
+    "url": "/img/youtube.7a26dadb.svg"
+  },
+  {
+    "revision": "843d37f6fdcf5f30f86ecfd8516cdca4",
+    "url": "/img/vk.843d37f6.svg"
+  },
+  {
+    "revision": "65d66274bf0fe4e8342c32315ede580d",
+    "url": "/img/stats-sub.65d66274.svg"
+  },
+  {
+    "revision": "f103a3cfc211cad88746467788abb37f",
+    "url": "/img/success-white.f103a3cf.svg"
+  },
+  {
+    "revision": "e692a03efd6b60f831695f11a7b3fb7d",
+    "url": "/img/next.e692a03e.svg"
+  },
+  {
+    "revision": "184e2b4e3b4ad745ab8de66ff0c88820",
+    "url": "/img/prev.184e2b4e.svg"
+  },
+  {
+    "revision": "993aa5cb0d92b4aa2419184405309858",
+    "url": "/img/logout.993aa5cb.svg"
+  },
+  {
+    "revision": "d09e7631ebf913c00f957b9fa5c75138",
+    "url": "/img/popular-ser.d09e7631.svg"
+  },
+  {
+    "revision": "733a009b8ac668f997835e5579de2bdc",
+    "url": "/img/star.733a009b.svg"
+  },
+  {
+    "revision": "1812ed6f5178e5ca22c87a257a2edd72",
+    "url": "/img/videos.1812ed6f.svg"
+  },
+  {
+    "revision": "d4286ba166e86891a05cf73d5fac1b5d",
+    "url": "/img/share.d4286ba1.svg"
+  },
+  {
+    "revision": "5b575f1030a9cd9ede1a0e06ac7a76ae",
+    "url": "/img/wheel.5b575f10.svg"
+  },
+  {
+    "revision": "bd313fb13beabc0652768c28cd1bd5b8",
+    "url": "/img/notifications.bd313fb1.svg"
+  },
+  {
+    "revision": "059036039b1b6d766c6a5ef83ff9d9d7",
+    "url": "/img/stats-com.05903603.svg"
+  },
+  {
+    "revision": "5b56660fc7a27baa38d66ec6600ce525",
+    "url": "/img/stories.5b56660f.svg"
+  },
+  {
+    "revision": "394fad4c16e7e30ea41563e114fa411b",
+    "url": "/img/tasks.394fad4c.svg"
+  },
+  {
+    "revision": "d876d8123b828637e7f15d196bd6496e",
+    "url": "/img/stats-likes.d876d812.svg"
+  },
+  {
+    "revision": "394fad4c16e7e30ea41563e114fa411b",
+    "url": "/img/up-arrow.394fad4c.svg"
+  },
+  {
+    "revision": "94f7bc096186e69700e44b452ec28e81",
+    "url": "/img/popular-aud.94f7bc09.svg"
+  },
+  {
+    "revision": "32446576734792a827147b052dbf5fae",
+    "url": "/img/popular-act.32446576.svg"
+  },
+  {
+    "revision": "5714d62b3d1280ebd0fbcfebf8009a50",
+    "url": "/img/popular-upme.5714d62b.svg"
+  },
+  {
+    "revision": "f7927b827db3f2cfd478",
+    "url": "/js/app.40d73636.js"
+  },
+  {
+    "revision": "75c7791b91fde704ffb798416e7e5831",
+    "url": "/img/subscribers.75c7791b.svg"
+  },
+  {
+    "revision": "05fb6bae0060a431ed74592203a2ea57",
+    "url": "/img/tg.05fb6bae.svg"
+  },
+  {
+    "revision": "0ff7d04f53aa9d19270af3030dc890b2",
+    "url": "/img/success.0ff7d04f.svg"
+  },
+  {
+    "revision": "8121e176227ae0835cdaabff0b5d2838",
+    "url": "/img/question.8121e176.svg"
+  },
+  {
+    "revision": "bf1a9dba911796e54b5c8ea1a4f56799",
+    "url": "/img/popular-con.bf1a9dba.svg"
+  },
+  {
+    "revision": "4f63e322c46a5031ea6d95dc7927d270",
+    "url": "/img/popular-line.4f63e322.svg"
+  },
+  {
+    "revision": "a79c0e06b875f547d5364ceb04ce8f62",
+    "url": "/img/error-white.a79c0e06.svg"
+  },
+  {
+    "revision": "0ab5bd41d7504d15287b99d6b83517a9",
+    "url": "/img/exchange.0ab5bd41.svg"
+  },
+  {
+    "revision": "0ba24d28b1eaa3c6692ef0a0f8606a71",
+    "url": "/img/giveaway.0ba24d28.svg"
+  },
+  {
+    "revision": "c8f11790182cb80a374d221f53c0bc94",
+    "url": "/img/likes.c8f11790.svg"
+  },
+  {
+    "revision": "f7613dc5c5b73b5e26a74ba78789ff11",
+    "url": "/img/location.f7613dc5.svg"
+  },
+  {
+    "revision": "a4a22121cb1944286e80fa5089dec5a5",
+    "url": "/img/involvement-top-background.a4a22121.png"
+  },
+  {
+    "revision": "9f96906f0cb82f923e0ee2a1b9f622d8",
+    "url": "/img/fb.9f96906f.svg"
+  },
+  {
+    "revision": "050ca25435b4085f59968d69feace58a",
+    "url": "/img/likes.050ca254.svg"
+  },
+  {
+    "revision": "3c6a617003aee4bd543e0fdecb2a4b77",
+    "url": "/img/gift.3c6a6170.svg"
+  },
+  {
+    "revision": "1e402e96f35f7e045ec7ec83ccfb2334",
+    "url": "/img/lives.1e402e96.svg"
+  },
+  {
+    "revision": "9ae8da869c23731c88213f41df63d357",
+    "url": "/img/involvement-bottom-background.9ae8da86.png"
+  },
+  {
+    "revision": "5471dab182f39c9f6e98c057cefa8471",
+    "url": "/img/heart.5471dab1.svg"
+  },
+  {
+    "revision": "d07d59a4781023441cc95c853f0cf3c0",
+    "url": "/img/home.d07d59a4.svg"
+  },
+  {
+    "revision": "797c4e82023341218b4d5968939f519c",
+    "url": "/img/inst.797c4e82.svg"
+  },
+  {
+    "revision": "73a9da54186d50999619537045ca8cd5",
+    "url": "/img/likes.73a9da54.svg"
+  },
+  {
+    "revision": "04c61d4168fc4fc3ccbf6c0dd6474974",
+    "url": "/img/edit.04c61d41.svg"
+  },
+  {
+    "revision": "aa9ccbe98d08440f30d073666a0cf341",
+    "url": "/img/comments.aa9ccbe9.svg"
+  },
+  {
+    "revision": "06e955aa48315a30ae8bc0d8f62f7463",
+    "url": "/img/content.06e955aa.svg"
+  },
+  {
+    "revision": "296d2f6840a3e910b912740151d01826",
+    "url": "/img/contacts.296d2f68.svg"
+  },
+  {
+    "revision": "e09600172628353b709614524c0a8e53",
+    "url": "/img/comments.e0960017.svg"
+  },
+  {
+    "revision": "0b864922e1d4adb041be47b73abaf9a7",
+    "url": "/img/content.0b864922.svg"
+  },
+  {
+    "revision": "d1cec4cd7be08850de83afbf8de85729",
+    "url": "/img/check.d1cec4cd.svg"
+  },
+  {
+    "revision": "9ebc8a888df090203a7fe7c8a0d5a08d",
+    "url": "/img/cam.9ebc8a88.svg"
+  },
+  {
+    "revision": "97835cc5eafa0e12b51bfd491322101f",
+    "url": "/img/banner-decor.97835cc5.svg"
+  },
+  {
+    "revision": "06f724c8305f417ddb95f8ca83ee4408",
+    "url": "/img/banner-decor-lg.06f724c8.svg"
+  },
+  {
+    "revision": "3fad92f6b9f0edcf67c93f42189ac008",
+    "url": "/img/age.3fad92f6.svg"
+  },
+  {
+    "revision": "f7927b827db3f2cfd478",
+    "url": "/css/app.2f85ca96.css"
+  },
+  {
+    "revision": "233101b25c611a897caee5fbd06026b1",
+    "url": "/CNAME"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.7c81601c8b8b7565b8041baa3d5b175e.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "upme"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
